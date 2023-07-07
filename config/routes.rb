9255377e51035d1c616bedd041325748181dc5b2
kevin collier <collier.kevin.m@gmail.com>
2023-07-07 13:47:18 +0900

    created bookmark list movie models

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :create, :new, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
+
+
+# I can see all my movie lists                (lists#index)
+# I can create a movie list                   (lists#create and lists#new)
+# I can see the details of a movie list       (lists#show)
+# I can bookmark a movie inside a movie list  (bookmarks#create and bookmarks#new)
+# I can destroy a bookmark                    (bookmarks#destroy)
+
+# R -> index
+# C -> create
+# C -> new
+# U -> edit
+# R -> show
+# U -> update
+# D -> destroy
